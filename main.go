6dadc3a16543cef601e992eaec503cd6a857da7d
dezhizhang <1541609448@qq.com>
2022-02-20 15:54:42 +0800

    feat:启动服务

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "electricity-web/initialize"
+import (
+	"electricity-web/initialize"
+	"fmt"
+	"go.uber.org/zap"
+)
 
 func main()  {
+	port := 8080
+	logger,_ := zap.NewProduction()
+	zap.ReplaceGlobals(logger)
 	//初始化路由
-	initialize.Routers()
+	Router := initialize.Routers()
+
+	zap.S().Debugf("启动服务器,端口:%d",port)
+	//启动服务
+	err := Router.Run(fmt.Sprintf(":%d",port))
+	if err != nil {
+		zap.S().Panic("启动服务失败",err.Error())
+	}
 }
